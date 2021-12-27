5315d14b1ee9c60c1d047ac566855cb859add826
Gabriel Silva <devzgabriel@gmail.com>
2021-12-27 17:30:48 -0300

    2.1.0

--- src/date/manipulation.ts
+++ src/date/manipulation.ts
@@ -7,18 +7,22 @@ export const parseDateToString = (date: Date) => String(date);
 
 export const parseStringToDate = (date: string) => new Date(date);
 
-export const formatDateToISO = (date: Date, dateFormat = 'yyyy-MM-dd') =>
-  parseISO(format(date, dateFormat));
+export const formatDateToISO = (
+  date: Date | number | string,
+  dateFormat = 'yyyy-MM-dd'
+) => parseISO(format(new Date(date), dateFormat));
 
 export const parseDateToStringWithoutHours = (
   date: Date,
   dateFormat = 'yyyy-MM-dd'
 ) => format(date, dateFormat);
 
-export const formatOnlyDate = (date: Date | number, pattern = 'yyyy/MM/dd') =>
-  format(date, pattern);
+export const formatOnlyDate = (
+  date: Date | string | number,
+  pattern = 'yyyy/MM/dd'
+) => format(new Date(date), pattern);
 
 export const formatDateTime = (
-  date: Date | number,
+  date: Date | number | string,
   pattern = 'yyyy/MM/dd - HH:mm'
-) => format(date, pattern);
+) => format(new Date(date), pattern);
