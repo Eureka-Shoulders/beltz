5e8d8b5884e7dbc0acfc311c47e36dccf7adbb79
Gabriel Silva <devzgabriel@gmail.com>
2021-12-30 13:19:17 -0300

    chore: add date presets

---
+++ src/date/presets.ts
@@ -0,0 +1,17 @@
+export const datePresets = {
+  fullDate: 'dd/MM/yy',
+  shortDate: 'dd/MM',
+  longDate: 'dd MMMM yyyy',
+
+  fullDateTime: 'dd/MM/yy HH:mm',
+  shortDateTime: 'dd/MM HH:mm',
+  longDateTime: 'dd MMMM yyyy HH:mm',
+
+  fullDateTimeSeconds: 'dd/MM/yy HH:mm:ss',
+  shortDateTimeSeconds: 'dd/MM HH:mm:ss',
+  longDateTimeSeconds: 'dd MMMM yyyy HH:mm:ss',
+
+  fullDateTimeMilliseconds: 'dd/MM/yy HH:mm:ss.SSS',
+  shortDateTimeMilliseconds: 'dd/MM HH:mm:ss.SSS',
+  longDateTimeMilliseconds: 'dd MMMM yyyy HH:mm:ss.SSS',
+};
